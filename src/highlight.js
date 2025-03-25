df6f68911c9ca7d23794e4fe421f0c62550a4cce
Jeff Dyer <jeffdyer@acm.org>
2025-03-25 10:17:16 -0700

    Add let definitions and terminators

--- src/highlight.js
+++ src/highlight.js
@@ -1,12 +1,14 @@
 import {styleTags, tags as t} from "@lezer/highlight"
 
 export const graffiticodeHighlighting = styleTags({
+  Let: t.keyword,
+  Terminator: t.separator,
   String: t.string,
   Number: t.number,
   "True False": t.bool,
   PropertyName: t.propertyName,
   Null: t.null,
-  ", :": t.separator,
+  ", : =": t.separator,
   "[ ]": t.squareBracket,
   "{ }": t.brace
 })
